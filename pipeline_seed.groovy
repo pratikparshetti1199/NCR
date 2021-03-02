a05f43f09a66aeabb2c2cf2a423573fa078a1198
pratikparshetti1199 <73302856+pratikparshetti1199@users.noreply.github.com>
2021-03-02 19:50:18 +0530

    First commit to groovy

---
+++ pipeline_seed.groovy
@@ -0,0 +1,28 @@
+pipelineJob('ESS/core_cxp-core-webapp-build-PR-Triggered') {
+    description 'Pipeline Job to build cxp-core-webapp of PRs branch through PR trigger'
+    }
+  
+    configure {
+		it / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty' {
+			'projectUrl'('https://github.com/ncr-swt-banking/cxp-core-webapp/')
+			displayName()
+		}
+	}
+
+    parameters {
+        stringParam('branch', '$ghprbSourceBranch', 'PRs branches')
+//        booleanParam('forceDeploy', false, 'force a deploy, even if not coming from master/develop')
+    }
+
+/*    configure {
+        project -> (project / 'authToken').setValue('cxpcorewebappbuildtoken')
+    }
+*/
+    definition {
+        def pipelineScript = 
+        cps {
+            script(pipelineScript)
+            sandbox()
+        }
+    }
+}
