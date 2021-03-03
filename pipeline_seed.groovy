1933a6a8edfc3600a9e9aec35080bfe6bef911fb
pratikparshetti1199 <73302856+pratikparshetti1199@users.noreply.github.com>
2021-03-03 11:52:18 +0530

    Check working of pipeline

--- pipeline_seed.groovy
+++ pipeline_seed.groovy
@@ -1,25 +1,27 @@
-pipelineJob('ESS/core_cxp-core-webapp-build-PR-Triggered') {
+pipelineJob('cxp-core-webapp-build-PR-Triggered') {
     description 'Pipeline Job to build cxp-core-webapp of PRs branch through PR trigger'
-    }
-  
+    
     configure {
-		it / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty' {
-			'projectUrl'('https://github.com/ncr-swt-banking/cxp-core-webapp/')
-			displayName()
-		}
-	}
+        it / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty' {
+          'projectUrl'('https://github.com/ncr-swt-banking/cxp-core-webapp/')
+        }
+    }
 
     parameters {
         stringParam('branch', '$ghprbSourceBranch', 'PRs branches')
-//        booleanParam('forceDeploy', false, 'force a deploy, even if not coming from master/develop')
     }
 
-/*    configure {
-        project -> (project / 'authToken').setValue('cxpcorewebappbuildtoken')
+
+    triggers {
+        githubPullRequest {
+            orgWhitelist('ncr-swt-banking')
+            cron('H/5 * * * *')         //This is the default command 
+            useGitHubHooks()
+        }
     }
-*/
+
     definition {
-        def pipelineScript = 'https://github.com/pratikparshetti1199/NCR.git/pipeline_seed.txt'
+        def pipelineScript = readFileFromWorkspace('pipeline_seed.txt')
         cps {
             script(pipelineScript)
             sandbox()
